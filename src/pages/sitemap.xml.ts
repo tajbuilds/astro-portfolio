650c0e2259c93edec0afaf74513c5d754e546bdc
Taj S <taj@fredolsentravel.cloud>
2026-02-28 14:11:23 +0000

    Serve valid XML at /sitemap.xml instead of redirect

--- src/pages/sitemap.xml.ts
+++ src/pages/sitemap.xml.ts
@@ -1,5 +1,18 @@
 import type { APIRoute } from 'astro';
 
-export const GET: APIRoute = ({ redirect }) => {
-	return redirect('/sitemap-index.xml', 301);
+export const GET: APIRoute = ({ request }) => {
+	const origin = new URL(request.url).origin;
+	const xml = `<?xml version="1.0" encoding="UTF-8"?>
+<sitemapindex xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <sitemap>
+    <loc>${origin}/sitemap-index.xml</loc>
+  </sitemap>
+</sitemapindex>`;
+
+	return new Response(xml, {
+		headers: {
+			'content-type': 'application/xml; charset=utf-8',
+			'cache-control': 'public, max-age=3600',
+		},
+	});
 };
